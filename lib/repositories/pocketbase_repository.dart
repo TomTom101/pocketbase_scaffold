171455d8445584f93d90d1f56f53f949c5565fae
TomTom101 <thobra@gmail.com>
2022-11-22 12:23:06 +0100

    Refactored, catch errors on create/update

--- lib/repositories/pocketbase_repository.dart
+++ lib/repositories/pocketbase_repository.dart
@@ -4,6 +4,48 @@ import 'package:pocketbase/pocketbase.dart';
 import '../models/pocketbase_model.dart';
 import '../provider/pocketbase_provider.dart';
 
+typedef JsonMap = Map<String, dynamic>;
+
+class JsonConverter {
+  static JsonMap toRecordModelJson(
+    BaseModel model, {
+    List<String>? relations,
+  }) {
+    var json = model.toJson();
+    for (final relation in relations ?? []) {
+      final relKey = json[relation];
+      if (relKey is BaseModel) {
+        json[relation] = json[relation].id;
+      }
+      // TODO untested
+      if (relKey is List<BaseModel>) {
+        json[relation] = (json[relation] as List<BaseModel>)
+            .map((BaseModel i) => i.id)
+            .toList();
+      }
+    }
+    return json;
+  }
+
+  static JsonMap toBaseModelJson(
+    RecordModel model, {
+    List<String>? relations,
+  }) {
+    var json = model.toJson();
+    if (json.containsKey("expand") && (json['expand'] as Map).isNotEmpty) {
+      (json['expand'] as Map).forEach((field, data) => json[field] = data);
+      json.remove("expand");
+    } else {
+      for (final relation in relations ?? []) {
+        if ((json[relation] as String).isEmpty) {
+          json.remove(relation);
+        }
+      }
+    }
+    return json;
+  }
+}
+
 final collectionServiceProvider = Provider.family<RecordService, String>(
     (ref, collection) => ref.watch(pocketBaseProvider).collection(collection));
 
@@ -18,54 +60,32 @@ abstract class PocketbaseRepository<T extends BaseModel>
 
   Future<Iterable> getAsMap() async {
     final data = await recordService.getFullList(expand: relations?.join(","));
-    return data.map((rm) {
-      var json = rm.toJson();
-      // if (rm.expand.isNotEmpty) {
-      //   rm.expand.forEach((key, value) => rm.data[key] = value);
-      // }
-
-      if (json.containsKey("expand") && (json['expand'] as Map).isNotEmpty) {
-        (json['expand'] as Map).forEach((field, data) => json[field] = data);
-        json.remove("expand");
-      } else {
-        for (final relation in relations ?? []) {
-          if ((json[relation] as String).isEmpty) {
-            json.remove(relation);
-          }
-        }
-      }
-      return json;
-    });
+    return data
+        .map((rm) => JsonConverter.toBaseModelJson(rm, relations: relations));
   }
 
-  Map<String, dynamic> _convertToPbBody(BaseModel model) {
-    var j = model.toJson();
-    // TODO move to private function
-    for (final relation in relations ?? []) {
-      final relKey = j[relation];
-      if (relKey is BaseModel) {
-        j[relation] = j[relation].id;
-      }
-      // TODO untested
-      if (relKey is List<BaseModel>) {
-        j[relation] = (j[relation] as List<BaseModel>)
-            .map((BaseModel i) => i.id)
-            .toList();
-      }
+  Future<String?> create(T model) async {
+    /// Returns an error string, null for success
+    try {
+      await recordService.create(
+        body: JsonConverter.toRecordModelJson(model),
+      );
+      getAll(loading: false);
+    } on ClientException catch (e) {
+      return "${e.response['message']}: ${e.response['data']}";
     }
-    return j;
+    return null;
   }
 
-  Future<String> create(T model) async {
-    final j = _convertToPbBody(model);
-    final rec = await recordService.create(body: j);
-    getAll(loading: false);
-    return rec.id;
-  }
-
-  Future<void> update(T model) async {
-    final j = _convertToPbBody(model);
-    await recordService.update(model.id!, body: j);
+  Future<String?> update(T model) async {
+    try {
+      await recordService.update(
+        model.id!,
+        body: JsonConverter.toRecordModelJson(model),
+      );
+    } on ClientException catch (e) {
+      return "${e.response['message']}: ${e.response['data']}";
+    }
     getAll(loading: false);
   }
 
